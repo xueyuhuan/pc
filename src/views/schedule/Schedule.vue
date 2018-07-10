b66d2460d94f25fb791a9aae3691ba36f5babef1
xus <754124808@qq.com>
2018-07-10 17:27:58 +0800

    日程

--- src/views/schedule/Schedule.vue
+++ src/views/schedule/Schedule.vue
@@ -4,20 +4,43 @@
             <div><i class="fa fa-calendar"></i>&nbsp;&nbsp;&nbsp;日程 <span>Schedule</span></div>
         </subhead>
         <div class="content">
+            <card>
+                <header slot="header">
+                    <div><i class="fa fa-chevron-left"></i>&nbsp;2018年8月<i class="fa fa-chevron-right"></i></div>
+                    <div>
+                        <div class="eventType" v-for="item in eventType">
+                            <span :style="'background-color:' + item.color + ';'"></span>{{item.name}}
+                        </div>
+                    </div>
+                </header>
 
+            </card>
         </div>
-        <card>
-            <header slot="header">
-                <div><i class="fa fa-chevron-left"></i>&nbsp;2018年8月<i class="fa fa-chevron-right"></i></div>
-            </header>
 
-        </card>
     </div>
 </template>
 
 <script>
     export default {
-        name: "Schedule"
+        name: "Schedule",
+        data(){
+            return{
+                eventType:[]
+            }
+
+        },
+        methods:{
+            //获取事件类型
+            getEventType(){
+                this.$ajax.post(this.$url.getCalendarObjs)
+                    .then(res => {
+                        this.eventType = res.data.cals;
+                    })
+            }
+        },
+        created(){
+            this.getEventType();
+        }
     }
 </script>
 
@@ -26,5 +49,8 @@
         padding: 0 0 15px 0;
         margin: auto;
         width: 1200px;
+    }
+    .eventType{
+
     }
 </style>
