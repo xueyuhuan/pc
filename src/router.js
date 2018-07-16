13a6e3e759da5d45c67eec03905d692fbcc57fea
xus <754124808@qq.com>
2018-07-16 18:42:11 +0800

    代办-一部分

--- src/router.js
+++ src/router.js
@@ -11,7 +11,8 @@ const news = () => import('./views/news/News');
 const newsDetail = () => import('./views/news/NewsDetail');
 const newsSub = () => import('./views/news/NewsSub');
 const schedule = () => import('./views/schedule/Schedule');
-const scheduleSub = () => import('./views/schedule/MySub')
+const scheduleSub = () => import('./views/schedule/MySub');
+const todo = () =>import('./views/todo/Todo');
 Vue.use(Router);
 
 export default new Router({
