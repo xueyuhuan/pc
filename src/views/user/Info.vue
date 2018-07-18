692dbb8fc2f635ad3802bcbbc044d1d828a22e28
xueyuhuan <501946152@qq.com>
2018-07-18 09:59:17 +0800

    url

---
+++ src/views/user/Info.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="info"></div>
+</template>
+
+<script>
+  export default {
+    name: "Info"
+  }
+</script>
+
+<style scoped lang="scss">
+    .info{
+        width: 1200px;
+        padding: 10px 0 50px;
+        margin: 15px auto 0;
+    }
+</style>
