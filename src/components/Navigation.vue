e43a27baf0fb2ab9211c0a4a0f405583eb37cf4e
xueyuhuan <501946152@qq.com>
2018-07-20 17:07:59 +0800

    导航图标

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -10,7 +10,7 @@
                 </ul>
             </nav>
             <ul class="personal">
-                <li @click="click('/feedback')" v-if="$school==='hit'">
+                <li @click="click('/feedback')" v-if="$school.school==='hit'">
                     <router-link to="/feedback">
                         <el-tooltip class="item" effect="light" content="意见反馈" placement="bottom">
                             <i class="fa fa-star-o" :class="{active:active==='/feedback'}"></i>
@@ -45,7 +45,7 @@
                             <i class="fa fa-star-o" :class="{active:active==='/collections'}"></i></el-tooltip>
                     </router-link>
                 </li>
-                <li @click="click('/message')" v-if="$school==='ccnu'">
+                <li @click="click('/message')" v-if="$school.school==='ccnu'">
                     <router-link to="/message">
                         <el-tooltip class="item" effect="light" content="消息中心" placement="bottom">
                             <el-badge :value="UnreadCount" :max="9" class="item" :hidden="UnreadCount === 0">
@@ -82,7 +82,6 @@
             }
         },
         created() {
-
             this.$ajax.post(this.$url.getUser)
                 .then(res => {
                     this.$store.commit('set_user', res.data.user);
