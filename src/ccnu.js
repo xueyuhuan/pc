37d5c9dc3179332a79e7c6dfb990c9fafc303e89
xueyuhuan <501946152@qq.com>
2018-07-27 14:36:00 +0800

    服务详情数据判断优化 正式环境存在问题：服务依据下载路径为空（目前版本已优化为不跳转）、线下服务亦显示线上办理（未优化） 未实现：判断是否已于系统整合（参数未知） 增加标题

--- src/ccnu.js
+++ src/ccnu.js
@@ -1,6 +1,7 @@
 export default {
   school:'ccnu',
   logo:'logo_ccnu.png',
+  enterName:'服务中心',
     copyright:'Copyright © 2017-2018 版权所有 华中师范大学信息化办公室',
     loginTitle:'华中师范大学校园门户',
   nav:[
--- src/hit.js
+++ src/hit.js
@@ -1,6 +1,11 @@
 export default {
   school:'hit',
   logo:'logo_hit.png',
+  title:'哈尔滨工业大学校园门户',
+  enterName:'师生服务中心',
+  enterNameEn:'Faculty & Students Service Center',
+  enterPlace:'一校区化工楼E栋一楼&nbsp;&nbsp;师生服务中心',
+  enterTime:'周一至周四：<br/>上午 8:00 - 11:30&nbsp;&nbsp;&nbsp;下午13:30 - 16:30<br/>周五：<br/>上午 8:00 - 11:30&nbsp;&nbsp;&nbsp;下午13:30 - 15:00',
     copyright:'Copyright © 2015-2018 哈尔滨工业大学网络与信息中心',
     loginTitle:'哈尔滨工业大学大学校园门户',
     nav:[
