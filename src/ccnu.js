8011fa902be613c7be7c18268a220eeee529e883
xueyuhuan <501946152@qq.com>
2018-08-08 16:53:26 +0800

    从后台拉取版权和登录标题

--- src/ccnu.js
+++ src/ccnu.js
@@ -4,8 +4,6 @@ export default {
   url:'/home',
   logo:'/img/ccnu/logo.png',
   enterName:'服务中心',
-    copyright:'Copyright © 2017-2018 版权所有 华中师范大学信息化办公室',
-    loginTitle:'华中师范大学校园门户',
   nav:[
     {name:'工作台',url:'/home'},
     {name:'服务',url:'/service'},
--- src/hit.js
+++ src/hit.js
@@ -7,8 +7,6 @@ export default {
   enterNameEn:'Faculty & Students Service Center',
   enterPlace:'一校区化工楼E栋一楼&nbsp;&nbsp;师生服务中心',
   enterTime:'周一至周四：<br/>上午 8:00 - 11:30&nbsp;&nbsp;&nbsp;下午13:30 - 16:30<br/>周五：<br/>上午 8:00 - 11:30&nbsp;&nbsp;&nbsp;下午13:30 - 15:00',
-    copyright:'Copyright © 2015-2018 哈尔滨工业大学网络与信息中心',
-    loginTitle:'哈尔滨工业大学大学校园门户',
     nav:[
     {name:'首页',url:'/index'},
     {name:'服务',url:'/service/search/icon'},
